6eb3479dec670ad3755fc7087147e6a4fc3d910f
Miswi <michal.swirski@student.uw.edu.pl>
2021-09-27 14:21:28 +0000

    Added rlang to manage envs and argument_control_functions.R to encapsulate argument controls and reduce boilerplate in main functions.

--- R/main_default_ORFikExp.R
+++ R/main_default_ORFikExp.R
@@ -35,54 +35,7 @@ multiOmicsPlot_ORFikExp <- function(target_range, annotation = target_range, df,
                                     plot_name = "default", plot_title = NULL, display_sequence = FALSE,
                                     annotation_names = NULL, start_codons = "ATG", stop_codons = c("TAA", "TAG", "TGA"),
                                     custom_motif = NULL) {
-  seqlevels(target_range) <- seqlevels(annotation)
-  target_range <- GRangesList(target_range)
-
-  # if (is(annotation, "GRangesList")) annotation <- unlist(annotation)
-
-  if (!is.null(annotation_names)) {
-    if (length(annotation_names) == 1){
-      if (annotation_names %in% annotation) {
-        names(annotation) <- mcols(annotation)[[annotation_names]]
-      } else stop(wmsg("wrong annotation_names argument"))
-    } else if (length(annotation_names) == length(annotation)) {
-      names(annotation) <- annotation_names
-    } else stop(wmsg("wrong annotation_names argument"))
-  }
-
-  if(!is(reads, "list")) reads <- list(reads)
-
-  if (length(withFrames) == 0) withFrames <- FALSE
-  if (!(length(withFrames)  %in% c(1, length(reads)))) stop("length of withFrames must be 0, 1 or the same as reads list")
-  if (length(withFrames) == 1) withFrames <- rep(withFrames, length(reads))
-
-  if (length(colors) == 0) colors <- 1:length(reads)
-  if (!(length(colors)  %in% c(1, length(reads)))) stop("length of colors must be 0, 1 or the same as reads list")
-  if (length(colors) == 1) colors <- rep(colors, length(reads))
-
-  if (length(kmers) == 0) kmers <- 1
-  if (!(length(kmers)  %in% c(1, length(reads)))) stop("length of kmers must be 0, 1 or the same as reads list")
-  if (length(kmers) == 1) kmers <- rep(kmers, length(reads))
-
-  if (length(ylabels) == 0) ylabels <- as.character(1:length(reads))
-  if (!(length(ylabels)  %in% c(1, length(reads)))) stop("length of ylabels must be 0, 1 or the same as reads list")
-  if (length(ylabels) == 1) ylabels <- rep(ylabels, length(reads))
-
-  if (length(proportions) == 0) proportions <- 1
-  if (!(length(proportions)  %in% c(1, length(reads)))) stop("length of proportions must be 0, 1 or the same as reads list")
-  if (length(proportions) == 1) proportions <- rep(proportions, length(reads))
-
-  if (!display_sequence) {
-    max_sum <- 1  - sum(0.03,0.1)
-    proportions <- proportions / (sum(proportions) / max_sum)
-    proportions <- c(proportions, c(0.03,0.1))
-  } else {
-    max_sum <- 1  - sum(0.035, 0.03,0.1)
-    proportions <- proportions / (sum(proportions) / max_sum)
-    proportions <- c(proportions, c(0.035, 0.03,0.1))
-  }
-
-
+  multiOmicsController()
   target_seq <- extractTranscriptSeqs(reference_sequence, target_range)
   seq_panel <- createSeqPanel(target_seq[[1]], start_codons = start_codons,
                               stop_codons = stop_codons, custom_motif = custom_motif)

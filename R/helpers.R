0a6c2fe9cac2eefb580b51188fccc7bfc53990fa
bartma11 <bartek.henkiel@gmail.com>
2021-11-03 23:56:15 +0100

    make fetchJS less magical

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,4 @@
-fetchJS <- function(filename) {
-    with_ext <- paste(filename, "js", sep = ".")
-    path_to_file <- here("js", with_ext)
+fetchJS <- function(path_to_file) {
     lines <- readLines(path_to_file)
     paste(lines, sep = "", collapse = "")
 }

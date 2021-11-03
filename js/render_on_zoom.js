dfe7981fabe1c24bc97a4c58f74f95952a9f9b7f
bartma11 <bartek.henkiel@gmail.com>
2021-11-03 23:55:52 +0100

    batch plotly operations

--- js/render_on_zoom.js
+++ js/render_on_zoom.js
@@ -1,6 +1,20 @@
 (elem, _, data) => {
-    let howManyTraces = elem.data.length;
-    let isTraceVisible = Array(data.length).fill(0);
+    let tracesVisible = 0;
+    let switchDistance = data[0]["distance"];
+    let tracesToAdd = data.map(traceDef => {
+        return {
+            "x": traceDef["x"],
+            "y": traceDef["y"],
+            "text": traceDef["text"],
+            "textfont": { "color": traceDef["color"] },
+            "xaxis": traceDef["xaxis"],
+            "yaxis": traceDef["yaxis"],
+            "mode": "text",
+        }
+    });
+    let indexesToDelete = Array(data.length).fill(0).map((_, index) => {
+        return index - data.length
+    });
     let onRelayout = (ed) => {
         if ("xaxis.range[0]" in ed && "xaxis.range[1]" in ed) {
             start = Math.floor(ed["xaxis.range[0]"]);
@@ -10,25 +24,14 @@
             end = Math.ceil(ed["width"]);
         }
         let distance = end - start;
-        data.forEach((traceDef, index) => {
-            if (distance <= traceDef["distance"] && isTraceVisible[index] == 0) {
-                let traceToAdd = {
-                    "x": traceDef["x"],
-                    "y": traceDef["y"],
-                    "text": traceDef["text"],
-                    "textfont": { "color": traceDef["color"] },
-                    "xaxis": traceDef["xaxis"],
-                    "yaxis": traceDef["yaxis"],
-                    "mode": "text",
-                };
-                Plotly.addTraces(elem, traceToAdd, howManyTraces + index);
-                isTraceVisible[index] = 1;
-            }
-            if (distance > traceDef["distance"] && isTraceVisible[index] == 1) {
-                Plotly.deleteTraces(elem, howManyTraces + index);
-                isTraceVisible[index] = 0;
-            }
-        })
+        if (distance <= switchDistance && tracesVisible == 0) {
+            Plotly.addTraces(elem, tracesToAdd);
+            tracesVisible = 1;
+        }
+        if (distance > switchDistance && tracesVisible == 1) {
+            Plotly.deleteTraces(elem, indexesToDelete);
+            tracesVisible = 0;
+        }
     };
     elem.on('plotly_relayout', onRelayout);
 }

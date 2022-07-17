b28687d8f80a8de99079ab2828eaa46117df0447
Donut96 <bartek.henkiel@gmail.com>
2022-07-17 14:08:29 +0000

    modified js handler for region selection so that it works with more than one plot/library

--- inst/js/select_region_on_click.js
+++ inst/js/select_region_on_click.js
@@ -1,9 +1,10 @@
 (elem, _, data) => {
+  let targetAxis = "y".concat(2 + data["nplots"]);
   let onClick = (ed) => {
     let point = ed["points"][0];
     let xaxis = point["data"]["xaxis"];
     let yaxis = point["data"]["yaxis"];
-    if (xaxis === "x" && yaxis === "y3" && "text" in point) {
+    if (xaxis === "x" && yaxis === targetAxis && "text" in point) {
       Shiny.setInputValue("selectedRegion", point["text"]);
     } else {
       Shiny.setInputValue("selectedRegion", null);

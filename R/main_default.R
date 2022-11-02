8d8ddc9fdee3b5a0af89b6515daf4a9a53441823
Miswi <michal.swirski@student.uw.edu.pl>
2022-11-02 16:26:23 +0000

    move multiOmicsPlot core to multiOmicsPlot_internal function

--- R/main_default.R
+++ R/main_default.R
@@ -63,6 +63,7 @@
 #' }
 multiOmicsPlot_list <- function(display_range, annotation = display_range, reference_sequence,
                                 reads, viewMode = c("tx", "genomic")[1], custom_regions = NULL,
+                                leader_extension = 0, trailer_extension = 0,
                                 withFrames = NULL,
                                 frames_type = "lines", colors = NULL,
                                 kmers = NULL, kmers_type = c("mean", "sum")[1],
@@ -74,68 +75,24 @@ multiOmicsPlot_list <- function(display_range, annotation = display_range, refer
                                 aa_letter_code = c("one_letter", "three_letters")[1],
                                 annotation_names = NULL,
                                 start_codons = "ATG", stop_codons = c("TAA", "TAG", "TGA"),
-                                custom_motif = NULL, AA_code = Biostrings::GENETIC_CODE,
+                                custom_motif = NULL,
                                 BPPARAM = bpparam()) {
-  multiOmicsController()
-  # Get sequence and create basic seq panel
-  target_seq <- extractTranscriptSeqs(reference_sequence, display_range)
-  seq_panel <- createSeqPanel(target_seq[[1]], start_codons = start_codons,
-                              stop_codons = stop_codons, custom_motif = custom_motif)
 
+  multiOmicsPlot_internal(display_range, df = NULL, annotation,reference_sequence,
+    reads,
+    viewMode,
+    custom_regions,
+    leader_extension, trailer_extension,
+    withFrames,
+    frames_type, colors, kmers, kmers_type,
+    ylabels, lib_to_annotation_proportions,lib_proportions,
+    annotation_proportions,width, height,
+    plot_name, plot_title,
+    display_sequence, seq_render_dist,
+    aa_letter_code,
+    annotation_names, start_codons, stop_codons,
+    custom_motif, BPPARAM)
 
-
-  gene_model_panel <- createGeneModelPanel(display_range, annotation,
-                                           custom_regions = custom_regions,
-                                           viewMode = viewMode)
-  lines <- gene_model_panel[[2]]
-  gene_model_panel <- gene_model_panel[[1]]
-  plots <- bpmapply(function(x,y,z,c,g) createSinglePlot(display_range, x,y,z,c,kmers_type, g, lines, type = frames_type),
-                    reads, withFrames, colors, kmers, ylabels, SIMPLIFY = FALSE, BPPARAM = BPPARAM)
-
-
-  if (display_sequence %in% c("none", FALSE)) { # plotly subplot without sequence track
-    plots <- c(plots, list(automateTicksGMP(gene_model_panel), automateTicksX(seq_panel)))
-    multiomics_plot <- subplot(plots,
-                               margin = 0,
-                               nrows = length(reads) + 2,
-                               heights = proportions,
-                               shareX = TRUE,
-                               titleY = TRUE,
-                               titleX = TRUE)
-  } else { # plotly subplot with sequence track
-    nplots <- length(plots)
-    nt_area <- ggplot() +
-      theme(axis.title = element_blank(),
-            axis.ticks = element_blank(),
-            axis.text = element_blank()) +
-      theme(plot.margin = unit(c(0,0,0,0), "pt"))+
-      scale_x_continuous(expand = c(0,0))
-
-    plots <- c(plots, list(automateTicks(nt_area), automateTicksGMP(gene_model_panel),
-                           automateTicksX(seq_panel)))
-    multiomics_plot <- subplot(plots,
-                               margin = 0,
-                               nrows = length(reads) + 3,
-                               heights = proportions,
-                               shareX = TRUE,
-                               titleY = TRUE,
-                               titleX = TRUE)
-    # Create sequence zoom logic (javascript)
-    display_dist <- nchar(target_seq)
-    js_data <- fetch_JS_seq(target_seq = target_seq, nplots = nplots,
-                            distance = seq_render_dist, display_dist = display_dist, aa_letter_code = aa_letter_code)
-    multiomics_plot <- onRender(multiomics_plot, RiboCrypt:::fetchJS("render_on_zoom.js"), js_data)
-  }
-
-  multiomics_plot <- multiomics_plot %>% plotly::config(
-    toImageButtonOptions = list(
-      format = "svg",
-      filename = ifelse(plot_name == "default", names(display_range), plot_name),
-      width = width,
-      height = height))
-  if (!is.null(plot_title)) multiomics_plot <- multiomics_plot %>% plotly::layout(title = plot_title)
-
-  return(multiomics_plot)
 }
 
 #' Multi-omics animation using list input
--- R/main_default_ORFikExp.R
+++ R/main_default_ORFikExp.R
@@ -43,82 +43,20 @@ multiOmicsPlot_ORFikExp <- function(display_range, df, annotation = "cds",refere
                                 aa_letter_code = c("one_letter", "three_letters")[1],
                                 annotation_names = NULL, start_codons = "ATG", stop_codons = c("TAA", "TAG", "TGA"),
                                 custom_motif = NULL, BPPARAM = BiocParallel::SerialParam()) {
-  multiOmicsController()
-  # Get sequence and create basic seq panel
-  target_seq <- extractTranscriptSeqs(reference_sequence, display_range)
-  seq_panel <- createSeqPanel(target_seq[[1]], start_codons = start_codons,
-                              stop_codons = stop_codons, custom_motif = custom_motif)
 
+  multiOmicsPlot_internal(display_range, df, annotation,reference_sequence,
+                          reads,
+                          viewMode,
+                          custom_regions,
+                          leader_extension, trailer_extension,
+                          withFrames,
+                          frames_type, colors, kmers, kmers_type,
+                          ylabels, lib_to_annotation_proportions,lib_proportions,
+                          annotation_proportions,width, height,
+                          plot_name, plot_title,
+                          display_sequence, seq_render_dist,
+                          aa_letter_code,
+                          annotation_names, start_codons, stop_codons,
+                          custom_motif, BPPARAM)
 
-  # Get the panel for the annotation track
-  gene_model_panel <- createGeneModelPanel(display_range, annotation,
-                                           custom_regions = custom_regions,
-                                           viewMode = viewMode)
-  lines <- gene_model_panel[[2]]
-  gene_model_panel <- gene_model_panel[[1]]
-  # Get NGS data tracks
-  force(display_range)
-  force(kmers_type)
-  force(lines)
-  force(frames_type)
-  force(reads)
-  force(withFrames)
-  force(colors)
-  force(kmers)
-  force(ylabels)
-  if (is(BPPARAM, "SerialParam")) {
-    plots <- mapply(function(x,y,z,c,g) createSinglePlot(display_range, x,y,z,c,kmers_type, g, lines, type = frames_type),
-                      reads, withFrames, colors, kmers, ylabels, SIMPLIFY = FALSE)
-  } else {
-    plots <- bpmapply(function(x,y,z,c,g) createSinglePlot(display_range, x,y,z,c,kmers_type, g, lines, type = frames_type),
-                      reads, withFrames, colors, kmers, ylabels, SIMPLIFY = FALSE, BPPARAM = BPPARAM)
-  }
-
-
-  if (display_sequence %in% c("none", FALSE)) { # plotly subplot without sequence track
-    plots <- c(plots, list(automateTicksGMP(gene_model_panel), automateTicksX(seq_panel)))
-    multiomics_plot <- subplot(plots,
-                               margin = 0,
-                               nrows = length(reads) + 2,
-                               heights = proportions,
-                               shareX = TRUE,
-                               titleY = TRUE,
-                               titleX = TRUE)
-  } else { # plotly subplot with sequence track
-    nplots <- length(plots)
-    nt_area <- ggplot() +
-      theme(axis.title = element_blank(),
-            axis.ticks = element_blank(),
-            axis.text = element_blank()) +
-      theme(plot.margin = unit(c(0,0,0,0), "pt"))+
-      scale_x_continuous(expand = c(0,0))
-
-    plots <- c(plots, list(automateTicks(nt_area), automateTicksGMP(gene_model_panel),
-                           automateTicksX(seq_panel)))
-    multiomics_plot <- subplot(plots,
-                               margin = 0,
-                               nrows = length(reads) + 3,
-                               heights = proportions,
-                               shareX = TRUE,
-                               titleY = TRUE,
-                               titleX = TRUE)
-    # Create sequence zoom logic (javascript)
-    display_dist <- nchar(target_seq)
-    render_on_zoom_data <- fetch_JS_seq(target_seq = target_seq, nplots = nplots,
-                            distance = seq_render_dist, display_dist = display_dist, aa_letter_code = aa_letter_code)
-    select_region_on_click_data <- list(nplots = nplots)
-    multiomics_plot <- multiomics_plot %>%
-      onRender(RiboCrypt:::fetchJS("render_on_zoom.js"), render_on_zoom_data) %>%
-      onRender(RiboCrypt:::fetchJS("select_region_on_click.js"), select_region_on_click_data)
-  }
-
-  multiomics_plot <- multiomics_plot %>% plotly::config(
-    toImageButtonOptions = list(
-      format = "svg",
-      filename = ifelse(plot_name == "default", names(display_range), plot_name),
-      width = width,
-      height = height))
-  if (!is.null(plot_title)) multiomics_plot <- multiomics_plot %>% plotly::layout(title = plot_title)
-
-  return(multiomics_plot)
 }

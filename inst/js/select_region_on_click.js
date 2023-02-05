c7819caa88788583e2f666bcd281d9f81aff947d
Donut <bartek.henkiel@gmail.com>
2023-02-05 16:35:45 +0000

    made script recognize diffrent event object shapes and extract required data acordingly

--- inst/js/select_region_on_click.js
+++ inst/js/select_region_on_click.js
@@ -5,8 +5,16 @@
     let point = ed["points"][0];
     let xaxis = point["data"]["xaxis"];
     let yaxis = point["data"]["yaxis"];
-    if (xaxis === "x" && yaxis === targetAxis && "text" in point["data"]) {
-      Shiny.setInputValue(data["input_id"], point["data"]["text"], {priority: "event"});
+    let region = null;
+    if ("text" in point["data"] && point["data"]["text"].constructor === String) {
+      region = point["data"]["text"];
+    } else if ("text" in point) {
+      region = point["text"];
+    } else {
+      region = null;
+    }
+    if (xaxis === "x" && yaxis === targetAxis && region !== null) {
+      Shiny.setInputValue(data["input_id"], region, {priority: "event"});
     } else {
       Shiny.setInputValue(data["input_id"], null);
     }

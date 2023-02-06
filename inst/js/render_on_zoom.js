434707b9af05f19e8774c4de0af165b4901527f6
Donut <bartek.henkiel@gmail.com>
2023-02-06 19:32:08 +0000

    made sequence render, render nt and aa sequence only visibile part of the plot +-200 nt

--- inst/js/render_on_zoom.js
+++ inst/js/render_on_zoom.js
@@ -1,17 +1,35 @@
 (elem, _, data) => {
   let tracesVisible = 0;
   let switchDistance = data[0]["distance"];
-  let tracesToAdd = data.map(traceDef => {
+  let tracesToAdd = (data, start, end) => { 
+    return data.map((traceDef, index) => {
+      let frame = index % 3;
+      
+      let startFrame = (start - 1) % 3;
+      let startShift = 0;
+      if (startFrame > frame) {
+        startShift = 1
+      };
+      let frameAdjustedStart = Math.floor(start / 3) + startShift;
+      
+      let endFrame = (end - 1) % 3;
+      let endShift = 0;
+      if (endFrame < frame) {
+        startShift = -1
+      };
+      let frameAdjustedEnd = Math.floor(end / 3) + endShift;
+      
       return {
-          "x": traceDef["x"],
-          "y": traceDef["y"],
-          "text": traceDef["text"],
+          "x": traceDef["x"].slice(frameAdjustedStart, frameAdjustedEnd),
+          "y": traceDef["y"].slice(frameAdjustedStart, frameAdjustedEnd),
+          "text": traceDef["text"].slice(frameAdjustedStart, frameAdjustedEnd),
           "textfont": { "color": traceDef["color"] },
           "xaxis": traceDef["xaxis"],
           "yaxis": traceDef["yaxis"],
           "mode": "text",
       }
-  });
+    });
+  };
   let indexesToDelete = Array(data.length).fill(0).map((_, index) => {
       return index - data.length
   });
@@ -25,7 +43,7 @@
       }
       let distance = end - start;
       if (distance <= switchDistance && tracesVisible == 0) {
-          Plotly.addTraces(elem, tracesToAdd);
+          Plotly.addTraces(elem, tracesToAdd(data, start - 100, end + 100));
           tracesVisible = 1;
       }
       if (distance > switchDistance && tracesVisible == 1) {

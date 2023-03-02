44bb243786ed9fe6a97cce36e7aa20e0ba914c95
Roleren <hauken_heyken@hotmail.com>
2023-03-02 13:49:55 +0100

    Cascade fixed, cleaned up test code

--- R/Module_controllers.R
+++ R/Module_controllers.R
@@ -55,17 +55,15 @@ study_and_gene_observers <- function(input, output, session) {
   with(rlang::caller_env(), {
     if (!exists("all_is_gene", mode = "logical")) all_is_gene <- FALSE
     if (!exists("uses_gene", mode = "logical")) uses_gene <- TRUE
-    if (!exists("idd", mode = "character")) idd <- NULL
+
     observe(if (rv$genome != input$genome & input$genome != "") {
-      print(paste("rv: input to genome", "browser"))
-      print(isolate(rv$genome))
-      print(isolate(input$genome))
       rv$genome <- input$genome},
             priority = 2) %>%
       bindEvent(input$genome, ignoreInit = TRUE, ignoreNULL = TRUE)
-    observe(if (rv$exp != input$dff & input$dff != "") {print("rv: input to exp");rv$exp <- input$dff}) %>%
+    observe(if (rv$exp != input$dff & input$dff != "") rv$exp <- input$dff) %>%
       bindEvent(input$dff, ignoreInit = TRUE, ignoreNULL = TRUE)
-    observe(if (rv$genome != input$genome) {print("rv: rvgenome to inputbox");updateSelectizeInput(
+    observe(if (rv$genome != input$genome) {
+      updateSelectizeInput(
       inputId = "genome",
       choices = c("ALL", unique(all_exp$organism)),
       selected = rv$genome,
@@ -74,14 +72,11 @@ study_and_gene_observers <- function(input, output, session) {
       bindEvent(rv$genome, ignoreInit = TRUE, ignoreNULL = TRUE)
 
     observeEvent(rv$exp, if (rv$exp != input$dff){
-      print("exp to inputbox")
       experiment_update_select(org, all_exp, experiments, rv$exp)},
                  ignoreInit = TRUE, ignoreNULL = TRUE)
 
     observeEvent(org(), if (org() != input$genome & input$genome != "") {
-      print("org to inputbox")
-      experiment_update_select(org, all_exp, experiments)
-    },
+      experiment_update_select(org, all_exp, experiments)},
                  ignoreInit = TRUE, ignoreNULL = TRUE)
     if (all_is_gene) {
       updateSelectizeInput(

f11811c9c4e98cbead8860c8c8cfc5cec7c96c68
Roleren <hauken_heyken@hotmail.com>
2024-06-07 13:19:35 +0200

    Added browseRC function

--- R/URL_helpers.R
+++ R/URL_helpers.R
@@ -216,3 +216,39 @@ check_url_for_go_on_init <- function() {
 
   })
 }
+
+#' Browse a gene on Ribocrypt webpage
+#'
+#' Can also disply local RiboCrypt app
+#' @param symbol gene symbol, default NULL
+#' @param gene_id gene symbol, default NULL
+#' @param tx_id gene symbol, default NULL
+#' @param exp experiment name, default "all_merged-Homo_sapiens_modalities"
+#' @param libraries NULL, default to first in experiment, c("RFP","RNA") would add RNA to default.
+#' @param host url, default "https://ribocrypt.org". Set to localhost for local version.
+#' @param plot_on_start logical, default TRUE. Plot gene when opening browser.
+#' @param frames_type "columns"
+#' @param kmer default 1 (kmer window to smear out reads)
+#' @param browser getOption("browser")
+#' @return browseURL, opens browse with page
+#' @export
+browseRC <- function(symbol = NULL, gene_id = NULL, tx_id = NULL, exp = "all_merged-Homo_sapiens_modalities",
+                     libraries=NULL,
+                     host = "https://ribocrypt.org", plot_on_start = TRUE,
+                     frames_type = "columns", kmer=1,
+                     browser = getOption("browser")) {
+  if (is.null(symbol) & is.null(gene_id)) stop("At least on of symbol and gene_id must be defined!")
+  exp <- paste0("&dff=", exp)
+  frames_type <- paste0("&frames_type=", frames_type)
+  kmer <- paste0("&kmer=", kmer)
+  prefix_url <- paste0(host, "/?", exp, frames_type, kmer)
+  gene <- paste0(if (!is.na(symbol) & !is.null(symbol)) {paste0(symbol, "-")} else NULL, gene_id)
+  if (!is.null(tx_id)) tx_id <- paste0("&tx=", tx_id)
+  if (!is.null(libraries)) libraries <- paste0("&library=", paste(libraries, collapse = ","))
+
+  select <- paste0("&gene=", gene, tx_id, libraries)
+  plot_on_start <- paste0("&go=", as.logical(plot_on_start))
+  full_url <- paste0(prefix_url, select, plot_on_start)
+  # full_url
+  browseURL(full_url, browser = browser)
+}

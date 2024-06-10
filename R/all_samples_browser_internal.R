ce2a4bfb25499320e899392e24a70cc186a6033c
Roleren <hauken_heyken@hotmail.com>
2024-06-10 10:29:44 +0200

    Split allsamples browser into cleaner subfunctions

--- R/all_samples_browser_internal.R
+++ R/all_samples_browser_internal.R
@@ -41,173 +41,83 @@ get_meta_browser_plot <- function(table, color_theme, clusters = 1,
                           show_heatmap_legend = FALSE)
 }
 
+#' Full plot for allsamples browser
+#'
+#' @param m data.table of coverage per sample (wide format)
+#' @param heatmap ComplexHeatmap object of plot from 'm'
+#' @param id id of transcript
+#' @param df ORFik experiment
+#' @param sumary logical, default TRUE (add top plot)
+#' @param annotation logical, default TRUE (add bottom annotation track)
+#' @param region_type character, "what is the coverage region?" Usually full mrna:
+#' "mrna" or "leader+cds".
+#' @param rel_heights numeric < 1, default: c(0.2, 0.75, 0.05).
+#' Relative heights, sum to 1 and must be length 3.
+#' @importFrom cowplot plot_grid
 get_meta_browser_plot_full <- function(m, heatmap, id, df,
                                        summary = TRUE, annotation = TRUE,
-                                       region_type = region_type,
+                                       region_type,
                                        rel_heights = c(0.2, 0.75, 0.05)) {
   stopifnot(is.numeric(rel_heights) && length(rel_heights) == 3)
   gene_model_panel <- summary_plot <- NULL
   if (!summary & !annotation) return(heatmap)
 
   if (summary) {
-    summary_track_type <- "area"
-    summary_profile <- data.table(count = rowSums(m))
-    summary_profile[, `:=`(position = seq.int(.N)) ]
-    summary_profile[, `:=`(frame = factor((position-1) %% 3)) ]
-    summary_plot <- RiboCrypt:::createSinglePlot(summary_profile, TRUE, 1, "",
-                                                 FALSE, lines = NULL,
-                                                 type = summary_track_type,
-                                                 flip_ylabel = FALSE, as_plotly = FALSE) +
-      theme(axis.text.y=element_blank(),  axis.ticks.y=element_blank()) ; summary_plot
+    summary_plot <- summary_track_allsamples(m)
   }
 
   if (annotation) {
-    lines <- NULL
-    withFrames <- TRUE
-    colors <- TRUE
-
-    lengths <- ORFik:::optimizedTranscriptLengths(df)
-    length <- lengths[tx_name == id]
-    # Custom UTR lengths for Sac cer (yeast)
-    if (organism(df) == "Saccharomyces cerevisiae")
-      length$utr5_len <- 650
-    tx_width <- ncol(heatmap) # length$tx_len
-
-    start <- 1
-    end <- tx_width
-
-    if (length$cds_len > 0 & region_type %in% c("mrna", "leader+cds")) {
-      start <- start + length$utr5_len
-      end <- start + length$cds_len
-    }
-    grl <- GRangesList(GRanges("1", IRanges(start, end)))
-    names(grl) <- id
-
-    ranges <- unlistGrl(grl)
-    ranges <- c(GRanges("1", IRanges(1, tx_width)), ranges)
-    dt <- geneBoxFromRanges(ranges, tx_width,
-                            cols = c("#FFFFFF", c("#F8766D","#00BA38","#619CFF")[start(ranges[-1]) %% 3 + 1]))[[1]]
-    gene_model_panel <- geneModelPanelPlot(dt)
+    gene_model_panel <- annotation_track_allsamples(df, id, region_type,
+                                                    tx_width = ncol(heatmap))
   }
 
   grob <- grid::grid.grabExpr(draw(heatmap))
   to_use_logicals <- c(summary, TRUE, annotation)
   final_plot <- cowplot::plot_grid(plotlist = list(summary_plot, grob, gene_model_panel)[to_use_logicals],
-                                   ncol = 1, rel_heights = c(0.2, 0.75, 0.05)[to_use_logicals])
+                                   ncol = 1, rel_heights = rel_heights[to_use_logicals])
   return(final_plot)
 }
 
-allsamples_metadata_clustering <- function(values, plot, numeric_bins = 5) {
-  time_before <- Sys.time()
-  print("Starting metabrowser clustering info")
-  at_least_2_values <- length(unique(values)) > 1
-  if (!at_least_2_values) message("Single value analysis not possible, skipping!")
+summary_track_allsamples <- function(m, summary_track_type = "area", as_plotly = FALSE) {
 
-  req(at_least_2_values)
-  pdf(NULL) # TODO: Make a better fix for blank pdf write
-  row_orders <- suppressWarnings(ComplexHeatmap::row_order(plot))
-  dev.off()
-  orders <- unlist(row_orders, use.names = FALSE)
-  clustering_was_done <- is.list(row_orders)
-
-  if (!clustering_was_done) {
-    orders <- order(values) # Order by variable instead of cluster
+  summary_profile <- data.table(count = rowSums(m))
+  summary_profile[, `:=`(position = seq.int(.N)) ]
+  summary_profile[, `:=`(frame = factor((position-1) %% 3)) ]
+  summary_plot <- createSinglePlot(summary_profile, TRUE, 1, "",
+                                   FALSE, lines = NULL,
+                                   type = summary_track_type,
+                                   flip_ylabel = FALSE, as_plotly = as_plotly)
+  if (!as_plotly) {
+    summary_plot +
+      theme(axis.text.y=element_blank(),  axis.ticks.y=element_blank())
   }
-
-  meta <- data.table(grouping = values, order = orders)
-  meta <- meta[meta$order, ]
-  if (clustering_was_done) {
-    meta[, cluster := rep(seq(length(row_orders)), lengths(row_orders))]
-  }
-  meta[, index := .I]
-
-  if (is.numeric(meta$grouping)) { #Make numeric bins
-    meta[, grouping_numeric_bins := cut(grouping, breaks=numeric_bins)]
-  }
-  enrich_dt <- allsamples_meta_stats(meta)
-  cat("metabrowser clustering info done"); print(round(Sys.time() - time_before, 2))
-  return(list(meta = meta, enrich_dt = enrich_dt))
+  return(summary_plot)
 }
 
-allsamples_sidebar <- function(meta) {
-  numeric_grouping <- is.numeric(meta$grouping)
+annotation_track_allsamples <- function(df, id, region_type, tx_width) {
+  lines <- NULL
+  withFrames <- TRUE
+  colors <- TRUE
 
-  gg <- ggplot(meta, aes(y = rev(index), x = factor(1), fill = grouping)) +
-    theme_void() +
-    labs(x = NULL, y = NULL, title = NULL) +
-    scale_x_discrete(expand = c(0, 0)) +
-    scale_y_continuous(expand = c(0, 0)) +
-    theme(panel.background = element_rect(fill = "transparent", colour = NA),
-          plot.background = element_rect(fill = "transparent", colour = NA),
-          panel.grid = element_blank(),
-          panel.border = element_blank(),
-          plot.margin = unit(c(0, 0, 0, 0), "cm"),
-          legend.position = "none")
+  lengths <- ORFik:::optimizedTranscriptLengths(df)
+  length <- lengths[tx_name == id]
+  # Custom UTR lengths for Sac cer (yeast)
+  if (organism(df) == "Saccharomyces cerevisiae")
+    length$utr5_len <- 650
 
-  if (numeric_grouping) {
-    meta[, grouping_numeric := grouping]
-    meta[, grouping := grouping_numeric_bins]
-    gg_tpm <- gg + geom_line(aes(y = grouping_numeric, x = rev(index), fill = NULL)) +
-      coord_flip()
-    plotly_tpm <- ggplotly(gg_tpm, tooltip="text")
+  start <- 1
+  end <- tx_width
+
+  if (length$cds_len > 0 & region_type %in% c("mrna", "leader+cds")) {
+    start <- start + length$utr5_len
+    end <- start + length$cds_len
   }
+  grl <- GRangesList(GRanges("1", IRanges(start, end)))
+  names(grl) <- id
 
-  gg_groups <- gg + geom_raster()
-  res <- ggplotly(gg_groups, tooltip="text")
-
-  if (numeric_grouping) {
-    res <- subplot(plotly_tpm, res, nrows = 1)
-  }
-  return(res %>% plotly::config(displayModeBar = FALSE))
+  ranges <- unlistGrl(grl)
+  ranges <- c(GRanges("1", IRanges(1, tx_width)), ranges)
+  dt <- geneBoxFromRanges(ranges, tx_width,
+                          cols = c("#FFFFFF", c("#F8766D","#00BA38","#619CFF")[start(ranges[-1]) %% 3 + 1]))[[1]]
+  return(geneModelPanelPlot(dt))
 }
-
-
-allsamples_meta_stats_shiny <- function(dt) {
-  # Add Chi squared significane coloring
-  datatable(round(dt, 2)) %>% formatStyle(columns = seq(ncol(dt)),
-                                          backgroundColor = styleInterval(c(-3, 3), c('yellow', 'white', 'yellow')))
-}
-
-allsamples_meta_stats <- function(meta) {
-  time_before <- Sys.time()
-  print("Starting metabrowser statistics")
-  res <- copy(meta)
-  res[, index := NULL]
-  if ("grouping_numeric_bins" %in% colnames(res)) {
-    res[, grouping := grouping_numeric_bins]
-  }
-  res[, grouping := as.character(grouping)]
-
-  clustering_was_done <- !is.null(res$cluster)
-  if (clustering_was_done) {
-    concat_table <- table(res$grouping, res$cluster)
-    chi_test <- chisq.test(concat_table)
-    res <- chi_test$stdres
-  } else {
-    concat_table <- table(res$grouping)
-    res <- matrix(concat_table, ncol = 1)
-    rownames(res) <- names(concat_table)
-    colnames(res) <- "counts"
-  }
-  cat("metabrowser statistics done"); print(round(Sys.time() - time_before, 2))
-  return(as.data.frame.matrix(res))
-}
-
-allsamples_enrich_bar_plot <- function(enrich) {
-  enrich_dt <- as.data.table(enrich, keep.rownames = T)
-  enrich_dt <- suppressWarnings(melt(enrich_dt))
-  enrich_dt[, variable := factor(as.character(variable))]
-  enrich_dt <- enrich_dt[rn != "",]
-  enrichment_plot <- ggplot(enrich_dt) +
-    geom_bar(aes(x = rn, y = value, fill = variable), stat="identity", position=position_dodge()) +
-    theme_minimal() + labs(fill = "Cluster") + xlab("Tissue") + ylab("Enrichment") + geom_hline(yintercept = c(3, -3), linetype="dashed",
-                                                                                                color = "red", linewidth=1) +
-    theme(axis.title = element_text(size = 32),
-          axis.text.x = element_text(size = (22), angle = 45),
-          axis.text.y = element_text(size = (22)),
-          legend.text = element_text(size = 22),
-          legend.title = element_text(size = 32))
-  return(enrichment_plot)
-}
-
-

c4d61503d9f2dc729f0ec64cca50becba0562227
Donut <bartek.henkiel@gmail.com>
2025-10-22 18:26:28 +0200

    Adjusted run_dev.R

--- run_dev.R
+++ run_dev.R
@@ -3,25 +3,25 @@
 devtools::load_all(".")
 
 all_exp <- list.experiments(
-    validate = FALSE,
-    BPPARAM = BiocParallel::SerialParam(),
-    dir = "~/Bio_data/ORFik_experiments_local"
+  validate = FALSE,
+  BPPARAM = BiocParallel::SerialParam(),
+  dir = "~/Bio_data/ORFik_experiments"
 )
 valid_collections <- c("all_samples-Homo_sapiens")
-m <- fread("~/Bio_data/NGS_pipeline/metadata_rc.csv")
+metadata <- fread("~/Bio_data/NGS_pipeline/metadata_rc.csv")
 all_exp <- all_exp[
-    name %in% c(grep("all_samples-", name, invert = TRUE, value = TRUE), valid_collections),
+  name %in% c(grep("all_samples-", name, invert = TRUE, value = TRUE), valid_collections),
 ]
 RiboCrypt_app(
-    FALSE,
-    all_exp = all_exp,
-    metadata = m,
-    browser_options = c(
-        default_experiment = "all_samples-Homo_sapiens",
-        default_gene = "ATF4-ENSG00000128272",
-        default_gene_meta = "ATF4-ENSG00000128272",
-        default_kmer = 9,
-        default_frame_type = "area",
-        plot_on_start = TRUE
-    )
+  FALSE,
+  all_exp = all_exp,
+  metadata = metadata,
+  browser_options = c(
+    default_experiment = "all_samples-Homo_sapiens",
+    default_gene = "ATF4-ENSG00000128272",
+    default_gene_meta = "ATF4-ENSG00000128272",
+    default_kmer = 9,
+    default_frame_type = "area",
+    plot_on_start = TRUE
+  )
 )

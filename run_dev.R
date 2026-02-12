d77fe5155635f9474839cb75ae5e73af0a638748
Donut <bartek.henkiel@gmail.com>
2026-02-12 13:28:44 +0100

    Added run script for development

---
+++ run_dev.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+devtools::load_all(".")
+
+all_exp <- list.experiments(
+  validate = FALSE,
+  BPPARAM = BiocParallel::SerialParam(),
+  dir = "~/Bio_data/ORFik_experiments"
+)
+valid_collections <- c("all_samples-Homo_sapiens")
+metadata <- fread("~/Bio_data/NGS_pipeline/metadata_rc.csv")
+all_exp_meta <- all_exp[
+  name %in% c(grep("all_samples-", name, invert = TRUE, value = TRUE), valid_collections),
+]
+RiboCrypt_app(
+  FALSE,
+  all_exp = all_exp_meta,
+  metadata = metadata,
+  browser_options = c(
+    default_experiment = "all_samples-Homo_sapiens",
+    default_gene = "ATF4-ENSG00000128272",
+    default_gene_meta = "ATF4-ENSG00000128272",
+    default_kmer = 9,
+    default_frame_type = "area",
+    plot_on_start = FALSE
+  )
+)
